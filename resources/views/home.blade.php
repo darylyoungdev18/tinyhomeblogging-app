b1ac89bf8c5ff0fffbc6961e6b8b60c02906bab7
darylyoung18 <darylyoung18755@gmail.com>
2024-10-16 22:17:11 -0400

    completed adding styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="min-h-screen bg-gray-50 dark:bg-gray-800 flex flex-col items-center justify-center text-gray-700 dark:text-gray-300">
+<div class="min-h-screen bg-gray-50 dark:bg-gray-800 flex flex-col items-center justify-center text-gray-700 dark:text-gray-300 bg-gradient-to-r from-cyan-500 to-blue-500">
     <div class="max-w-2xl p-6 text-center">
-        
+
         <h1 class="text-5xl font-bold mb-6">Welcome to Tiny Home Blogging!</h1>
         <p class="text-xl mb-4">
             This is the home page of the Tiny Home Blogging App. Start sharing your thoughts and insights today!
         </p>
-        <a href="/register" class="text-blue-500 hover:underline">Join us today</a>
+        <a href="/register" class="text-gray-900 hover:underline">Join us today</a>
     </div>
 </div>
 @endsection
